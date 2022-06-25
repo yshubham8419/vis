ac72054ae8adbd06be356f819e5005fb9d693d8a
yshubham8419 <yshubham592@gmail.com>
2022-06-25 23:42:42 +0530

    width of a tree

--- abcd.cpp
+++ abcd.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 #include <map>
 using namespace std;
-int maxlevel;
+int level;
 bool found;
 class node{
     public:
@@ -17,40 +17,73 @@ class node{
     }
 };
 void insertIn(node* root,int l,node** point){
-    if(l>maxlevel){
+    if(found)
+        return;
+    if(root->left && l<level){
+        insertIn(root->left,l+1,point);
+    }
+    if(found)
+        return;
+    if(root->right && l<level){
+        insertIn(root->right,l+1,point);
+    }
+    bool halfLeaf=!root->left || !root->right;
+    if(halfLeaf){
+        *point=root;
         found=true;
     }
 
-    if(found)
-        return;
-
-    bool halfleaf=!root->right || !root->left;
-
-    if(root->right && !halfleaf){
-        insertIn(root->right,l+1,point);
-    }
-
-    if(root->left && !halfleaf){
-        insertIn(root->left,l+1,point);
-    }
-
-    if(halfleaf && l<=maxlevel){
-        *point=root;
-        if(l<maxlevel)
-            maxlevel=l;
-    }
-
 }
-
+void printBT(const std::string& prefix, const node* root, bool isLeft)
+{
+    if (root != NULL)
+    {
+        std::cout << prefix;
+        std::cout << (isLeft ? "|--" : "L--");
+        // print the value of the node
+        std::cout << root->val << std::endl;
+        // enter the next tree level - left and right branch
+        printBT(prefix + (isLeft ? "|   " : "    "), root->right, true);
+        printBT(prefix + (isLeft ? "|   " : "    "), root->left, false);
+    }
+}
+int recursion(node* root,int *width){
+    int l,r;
+    bool leaf=true;
+    if(root->left){
+        l=recursion(root->left,width);
+        leaf=false;
+    }
+    else{
+        l=-1;
+    }
+    if(root->right){
+        r=recursion(root->right,width);
+        leaf=false;
+    }
+    else{
+        r=-1;
+    }
+    if(leaf)
+        return 0;
+    if(l+r+2>(*width))
+        *width=l+r+2;
+    return 1+max(l,r);
+}
 int main() { 
     int x,n;
     cin>>n>>x;
+    int t=n;
     node* root=new node(x);
-    n--;
-    while(n--){
-        node *point;
+    t--;
+    level=0;
+    while(t--){
+        node *point=NULL;
         found=false;
-        maxlevel=n;
+        
+        insertIn(root,0,&point);
+        if(point==NULL)
+            level++;
         insertIn(root,0,&point);
         cin>>x;
         node* temp=new node(x);
@@ -61,9 +94,10 @@ int main() {
             point->left=temp;
         }
     }
-    x=0;
-    x++;
-    n++;
+    int w=0;
+    printBT("",root,false);
+    recursion(root,&w);
+    cout<<w;
+
     return 0;
 }
-
